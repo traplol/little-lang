f1759a6b279c920533752c78b2205590e300887a
Max Mickey <max.mickey1991@gmail.com>
2015-08-25 23:47:36 -0500

    :lexer.h, parser.h - lexer/parser headers

---
+++ include/lexer.h
@@ -0,0 +1,8 @@
+#ifndef _LITTLE_LANG_LEXER_H
+#define _LITTLE_LANG_LEXER_H
+
+struct Lexer {
+    
+};
+
+#endif
---
+++ include/parser.h
@@ -0,0 +1,8 @@
+#ifndef _LITTLE_LANG_PARSER_H
+#define _LITTLE_LANG_PARSER_H
+
+struct Parser {
+    
+};
+
+#endif
