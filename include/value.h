df6df0611dec4d1ec4a552fada7b9ce11bceb9c1
Max Mickey <max.mickey1991@gmail.com>
2015-08-25 19:50:51 -0500

    :value.h - rethinking the object model.

--- include/value.h
+++ include/value.h
@@ -1,17 +1,29 @@
 #ifndef _LITTLE_LANG_VALUE_H
 #define _LITTLE_LANG_VALUE_H
 
-enum Type {
+enum TypeInfoType {
     TypeBaseObject,           /* The base object every object is derived from. */
     TypeInteger,              /* Raw integer type */
     TypeReal,                 /* Raw double type */
-    TypeUserObject            /* A user defined object */
+    TypeUserObject,           /* A user defined object */
+
+    TypeFunction,
+};
+
+struct TypeInfo {
+    enum TypeInfoType Type;
+    struct TypeInfo *DerivedFrom;
+    char *TypeName;           /* The name of the value's type, used for member lookup. */
+    unsigned int Size;
+    struct Member {
+        char *Name;
+        unsigned int Offset;
+        unsigned int Size;
+    } **Members;
 };
 
 struct Value {
-    enum Type Type;
-    char *TypeName;           /* The name of the value's type, used for member lookup. */
-    unsigned int SizeOfValue; /* Max should be the size of a pointer. */
+    struct TypeInfo TypeInfo;
     union {
         int Integer;
         double Real;
@@ -19,7 +31,9 @@ struct Value {
     } v;
 };
 
-
-int ValueMake(struct Value *value, enum Type type, void *data, unsigned int dataSize);
+/* Initializes the value. */
+int ValueMake(struct Value *value, struct TypeInfo typeInfo, void *data, unsigned int dataSize);
+/* Frees the value's data. */
+int ValueFree(struct Value *value);
 
 #endif
