fcc338c3082b338b97a97486a5ef70c6e566c0a0
Max Mickey <max.mickey1991@gmail.com>
2015-08-25 21:31:20 -0500

    :value.h - adding more meta data to values.

--- include/value.h
+++ include/value.h
@@ -13,18 +13,20 @@ enum TypeInfoType {
 
 struct TypeInfo {
     enum TypeInfoType Type;
-    struct TypeInfo *DerivedFrom;
-    char *TypeName;           /* The name of the value's type, used for member lookup. */
+    struct TypeInfo *DerivedFrom; /* All objects are derived from at least the BaseObject */
+    char *TypeName;
     unsigned int Size;
     struct Member {
         char *Name;
-        unsigned int Offset;
-        unsigned int Size;
+        struct TypeInfo *TypeInfo;
+        unsigned int MemOffset;
     } **Members;
+    unsigned int MembersLen;
+    unsigned int CurrentMemberIdx;
 };
 
 struct Value {
-    struct TypeInfo TypeInfo;
+    struct TypeInfo *TypeInfo;
     union {
         int Boolean;
         int Integer;
@@ -35,8 +37,20 @@ struct Value {
 };
 
 /* Initializes the value. */
-int ValueMake(struct Value *value, struct TypeInfo typeInfo, void *data, unsigned int dataSize);
+int ValueMake(struct Value *value, struct TypeInfo *typeInfo, void *data, unsigned int dataSize);
 /* Frees the value's data. */
 int ValueFree(struct Value *value);
+
+/* Initializes the type info. */
+int TypeInfoMake(struct TypeInfo *typeInfo, enum TypeInfoType type, struct TypeInfo *derivedFrom, char *typeName, unsigned int size);
+/* Frees the type info's data. */
+int TypeInfoFree(struct TypeInfo *typeInfo);
+/* Inserts a new member into the type info. */
+int TypeInfoInsertMember(struct TypeInfo *typeInfo, char *name, struct TypeInfo *memberTypeInfo, unsigned int memOffset);
+/* Lookup a member, returns a non-zero integer if the member was found and sets
+ * out_member to the found member. */
+int TypeInfoLookupMember(struct TypeInfo *typeInfo, char *name, struct Member *out_member);
+/* Returns a non-zero integer if the type contains the member matching the name. */
+int TypeInfoHasMember(struct TypeInfo *typeInfo, char *name);
 
 #endif
