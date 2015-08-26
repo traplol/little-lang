37e10728c76b269271e6284806fec169ea559c14
Max Mickey <max.mickey1991@gmail.com>
2015-08-25 23:48:11 -0500

    :little-lang-machine.h, little-lang.c - setting up state machine init

--- include/little-lang-machine.h
+++ include/little-lang-machine.h
@@ -1,11 +1,24 @@
 #ifndef _LITTLE_LANG_LITTLE_LANG_MACHINE_H
 #define _LITTLE_LANG_LITTLE_LANG_MACHINE_H
 
+#include "lexer.h"
+#include "parser.h"
+#include "table.h"
+
+/* TODO: Local stack, scoping, etc. */
 struct LittleLangMachine {
     struct Lexer *Lexer;
     struct Parser *Parser;
     struct Table *SymbolTable;
     struct Table *TypeTable;
-    struct CmdOptions *CmdOptions;
+    struct {
+        int argc;
+        char **argv;
+        char **Filenames;
+    } CmdOpts;
 };
+
+int LittleLangMachineInit(struct LittleLangMachine *llm, int argc, char **argv);
+int LittleLangMachineRun(struct LittleLangMachine *llm);
+
 #endif
--- src/little-lang.c
+++ src/little-lang.c
@@ -1,6 +1,12 @@
+#include "little-lang-machine.h"
+
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+    struct LittleLangMachine llm;
     --argc, ++argv;
-    return 0;
+    if (0 != LittleLangMachineInit(&llm, argc, argv)) {
+        exit(1);
+    }
+    return LittleLangMachineRun(&llm);
 }
