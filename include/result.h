880aff7755f3c54ceb172241c52c0f7510555a02
Max Mickey <max.mickey1991@gmail.com>
2015-09-05 22:43:34 -0500

    :result.h - added R_OperationFailed result.

--- include/result.h
+++ include/result.h
@@ -5,12 +5,13 @@
 #define R_True -1
 #define R_False 0
 
-#define R_OK 0
-#define R_InvalidArgument 1
-#define R_AllocFailed 2
+#define R_OK                0
+#define R_InvalidArgument   1
+#define R_AllocFailed       2
 #define R_KeyAlreadyInTable 3
-#define R_UnexpectedToken 4
+#define R_UnexpectedToken   4
 #define R_NotYetImplemented 5
+#define R_OperationFailed   6
 
 
 #endif
