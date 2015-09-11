4d910f215a273f8f18553a5a49ac0d85534f9178
Max Mickey <max.mickey1991@gmail.com>
2015-09-11 15:27:59 -0500

    :tests/src/ast_test.c - updated to use SrcLoc

--- tests/src/ast_test.c
+++ tests/src/ast_test.c
@@ -1,3 +1,4 @@
+#include "../src/globals.c"
 #include "../src/type_info.c"
 #include "../src/value.c"
 #include "../src/ast.c"
@@ -8,7 +9,9 @@
 struct Value *value;
 struct Ast *ast1, *ast2, *ast3, *ast4, *ast5;
 char *name;
+struct SrcLoc srcLoc = {"test.ll", -1, -1};
 void setup(void) {
+    GlobalsInit();
     value = malloc(sizeof *value);
     name = strdup("the_name");
 
@@ -31,96 +34,96 @@ void done(void) {
 
 TEST(AstMakeBoolean) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeBoolean(&ast, value), "AstMakeBoolean failed.");
-    assert_eq(R_InvalidArgument, AstMakeBoolean(&ast, NULL), "AstMakeBoolean did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeBoolean(NULL, value), "AstMakeBoolean did not fail.");
+    assert_eq(R_OK, AstMakeBoolean(&ast, value, srcLoc), "AstMakeBoolean failed.");
+    assert_eq(R_InvalidArgument, AstMakeBoolean(&ast, NULL, srcLoc), "AstMakeBoolean did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeBoolean(NULL, value, srcLoc), "AstMakeBoolean did not fail.");
     //AstFree(ast);
     free(ast);
 }
 
 TEST(AstMakeReal) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeReal(&ast, value), "AstMakeReal failed.");
+    assert_eq(R_OK, AstMakeReal(&ast, value, srcLoc), "AstMakeReal failed.");
     free(ast);
-    assert_eq(R_InvalidArgument, AstMakeReal(&ast, NULL), "AstMakeReal did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeReal(NULL, value), "AstMakeReal did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeReal(&ast, NULL, srcLoc), "AstMakeReal did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeReal(NULL, value, srcLoc), "AstMakeReal did not fail.");
 }
 
 TEST(AstMakeInteger) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeInteger(&ast, value), "AstMakeInteger failed.");
+    assert_eq(R_OK, AstMakeInteger(&ast, value, srcLoc), "AstMakeInteger failed.");
     free(ast);
-    assert_eq(R_InvalidArgument, AstMakeInteger(&ast, NULL), "AstMakeInteger did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeInteger(NULL, value), "AstMakeInteger did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeInteger(&ast, NULL, srcLoc), "AstMakeInteger did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeInteger(NULL, value, srcLoc), "AstMakeInteger did not fail.");
 }
 
 TEST(AstMakeString) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeString(&ast, value), "AstMakeString failed.");
+    assert_eq(R_OK, AstMakeString(&ast, value, srcLoc), "AstMakeString failed.");
     free(ast);
-    assert_eq(R_InvalidArgument, AstMakeString(&ast, NULL), "AstMakeString did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeString(NULL, value), "AstMakeString did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeString(&ast, NULL, srcLoc), "AstMakeString did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeString(NULL, value, srcLoc), "AstMakeString did not fail.");
 }
 
 TEST(AstMakeSymbol) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeSymbol(&ast, name), "AstMakeSymbol failed.");
+    assert_eq(R_OK, AstMakeSymbol(&ast, name, srcLoc), "AstMakeSymbol failed.");
     free(ast);
-    assert_eq(R_InvalidArgument, AstMakeSymbol(&ast, NULL), "AstMakeSymbol did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeSymbol(NULL, name), "AstMakeSymbol did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeSymbol(&ast, NULL, srcLoc), "AstMakeSymbol did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeSymbol(NULL, name, srcLoc), "AstMakeSymbol did not fail.");
 }
 
 TEST(AstMakeFunction) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeFunction(&ast, value), "AstMakeFunction failed.");
+    assert_eq(R_OK, AstMakeFunction(&ast, value, srcLoc), "AstMakeFunction failed.");
     free(ast);
-    assert_eq(R_InvalidArgument, AstMakeFunction(&ast, NULL), "AstMakeFunction did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeFunction(NULL, value), "AstMakeFunction did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeFunction(&ast, NULL, srcLoc), "AstMakeFunction did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeFunction(NULL, value, srcLoc), "AstMakeFunction did not fail.");
 }
 
 TEST(AstMakeBinaryOp) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeBinaryOp(&ast, ast1, BAddExpr, ast2), "AstMakeBinaryOp failed.");
+    assert_eq(R_OK, AstMakeBinaryOp(&ast, ast1, BAddExpr, ast2, srcLoc), "AstMakeBinaryOp failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeBinaryOp(NULL, ast1, BAddExpr, ast2), "AstMakeBinaryOp did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeBinaryOp(&ast, NULL, BAddExpr, ast2), "AstMakeBinaryOp did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeBinaryOp(&ast, ast1, BAddExpr, NULL), "AstMakeBinaryOp did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeBinaryOp(NULL, ast1, BAddExpr, ast2, srcLoc), "AstMakeBinaryOp did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeBinaryOp(&ast, NULL, BAddExpr, ast2, srcLoc), "AstMakeBinaryOp did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeBinaryOp(&ast, ast1, BAddExpr, NULL, srcLoc), "AstMakeBinaryOp did not fail.");
 }
 
 TEST(AstMakeUnaryOp) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeUnaryOp(&ast, UNegExpr, ast1), "AstMakeUnaryOp failed.");
+    assert_eq(R_OK, AstMakeUnaryOp(&ast, UNegExpr, ast1, srcLoc), "AstMakeUnaryOp failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeUnaryOp(NULL, UNegExpr, ast1), "AstMakeUnaryOp did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeUnaryOp(&ast, UNegExpr, NULL), "AstMakeUnaryOp did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeUnaryOp(NULL, UNegExpr, ast1, srcLoc), "AstMakeUnaryOp did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeUnaryOp(&ast, UNegExpr, NULL, srcLoc), "AstMakeUnaryOp did not fail.");
 }
 
 TEST(AstMakeAssign) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeAssign(&ast, ast1, ast2), "AstMakeAssign failed.");
+    assert_eq(R_OK, AstMakeAssign(&ast, ast1, ast2, srcLoc), "AstMakeAssign failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeAssign(NULL, ast1, ast2), "AstMakeAssign failed.");
-    assert_eq(R_InvalidArgument, AstMakeAssign(&ast, NULL, ast2), "AstMakeAssign did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeAssign(&ast, ast1, NULL), "AstMakeAssign did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeAssign(NULL, ast1, ast2, srcLoc), "AstMakeAssign failed.");
+    assert_eq(R_InvalidArgument, AstMakeAssign(&ast, NULL, ast2, srcLoc), "AstMakeAssign did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeAssign(&ast, ast1, NULL, srcLoc), "AstMakeAssign did not fail.");
 }
 
 TEST(AstMakeCall) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeCall(&ast, name, ast1), "AstMakeCall failed.");
+    assert_eq(R_OK, AstMakeCall(&ast, name, ast1, srcLoc), "AstMakeCall failed.");
     free(ast->Children[0]); free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeCall(&ast, name, NULL), "AstMakeCall with no args failed.");
+    assert_eq(R_OK, AstMakeCall(&ast, name, NULL, srcLoc), "AstMakeCall with no args failed.");
     free(ast->Children[0]); free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeCall(NULL, name, ast1), "AstMakeCall did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeCall(&ast, NULL, ast1), "AstMakeCall did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeCall(NULL, name, ast1, srcLoc), "AstMakeCall did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeCall(&ast, NULL, ast1, srcLoc), "AstMakeCall did not fail.");
 }
 
 TEST(AstMakeReturn) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeReturn(&ast, ast1), "AstMakeReturn failed.");
+    assert_eq(R_OK, AstMakeReturn(&ast, ast1, srcLoc), "AstMakeReturn failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeReturn(&ast, NULL), "AstMakeReturn with no value failed.");
+    assert_eq(R_OK, AstMakeReturn(&ast, NULL, srcLoc), "AstMakeReturn with no value failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeReturn(NULL, ast1), "AstMakeReturn did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeReturn(NULL, ast1, srcLoc), "AstMakeReturn did not fail.");
 }
 
 TEST(AstMakeMut) {
@@ -130,18 +133,18 @@ TEST(AstMakeMut) {
     AstAppendChild(tmp1, ast1);
     AstMakeBlank(&tmp2);
     AstAppendChild(tmp2, ast2);
-    assert_eq(R_OK, AstMakeMut(&ast, tmp1, tmp2), "AstMakeMut failed.");
+    assert_eq(R_OK, AstMakeMut(&ast, tmp1, tmp2, srcLoc), "AstMakeMut failed.");
     free(ast->Children); free(ast);
 
-    assert_eq(R_InvalidArgument, AstMakeMut(&ast, tmp1, ast2), "AstMakeMut should fail when either arg has no children");
-    assert_eq(R_InvalidArgument, AstMakeMut(&ast, ast1, tmp2), "AstMakeMut should fail when either arg has no children");
+    assert_eq(R_InvalidArgument, AstMakeMut(&ast, tmp1, ast2, srcLoc), "AstMakeMut should fail when either arg has no children");
+    assert_eq(R_InvalidArgument, AstMakeMut(&ast, ast1, tmp2, srcLoc), "AstMakeMut should fail when either arg has no children");
 
     AstAppendChild(tmp2, ast2);
-    assert_eq(R_InvalidArgument, AstMakeMut(&ast, ast1, tmp2), "AstMakeMut should fail when args don't have same number of children.");
+    assert_eq(R_InvalidArgument, AstMakeMut(&ast, ast1, tmp2, srcLoc), "AstMakeMut should fail when args don't have same number of children.");
 
-    assert_eq(R_InvalidArgument, AstMakeMut(NULL, tmp1, tmp2), "AstMakeMut did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeMut(&ast, NULL, tmp2), "AstMakeMut did not fail with no names.");
-    assert_eq(R_InvalidArgument, AstMakeMut(&ast, tmp1, NULL), "AstMakeMut did not fail with no names.");
+    assert_eq(R_InvalidArgument, AstMakeMut(NULL, tmp1, tmp2, srcLoc), "AstMakeMut did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeMut(&ast, NULL, tmp2, srcLoc), "AstMakeMut did not fail with no names.");
+    assert_eq(R_InvalidArgument, AstMakeMut(&ast, tmp1, NULL, srcLoc), "AstMakeMut did not fail with no names.");
 
     free(tmp1->Children);
     free(tmp1);
@@ -151,52 +154,52 @@ TEST(AstMakeMut) {
 
 TEST(AstMakeConst) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeConst(&ast, name, ast1), "AstMakeConst failed.");
+    assert_eq(R_OK, AstMakeConst(&ast, name, ast1, srcLoc), "AstMakeConst failed.");
     free(ast->Children[0]); free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeConst(NULL, name, ast1), "AstMakeConst did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeConst(&ast, NULL, ast1), "AstMakeConst did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeConst(&ast, name, NULL), "AstMakeConst did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeConst(NULL, name, ast1, srcLoc), "AstMakeConst did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeConst(&ast, NULL, ast1, srcLoc), "AstMakeConst did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeConst(&ast, name, NULL, srcLoc), "AstMakeConst did not fail.");
 }
 
 TEST(AstMakeFor) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, ast3, ast4), "AstMakeFor failed.");
+    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, ast3, ast4, srcLoc), "AstMakeFor failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeFor(&ast, NULL, ast2, ast3, ast4), "AstMakeFor failed with no pre.");
+    assert_eq(R_OK, AstMakeFor(&ast, NULL, ast2, ast3, ast4, srcLoc), "AstMakeFor failed with no pre.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeFor(&ast, ast1, NULL, ast3, ast4), "AstMakeFor failed with no condition.");
+    assert_eq(R_OK, AstMakeFor(&ast, ast1, NULL, ast3, ast4, srcLoc), "AstMakeFor failed with no condition.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, NULL, ast4), "AstMakeFor failed with no body.");
+    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, NULL, ast4, srcLoc), "AstMakeFor failed with no body.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, ast3, NULL), "AstMakeFor failed with no post.");
+    assert_eq(R_OK, AstMakeFor(&ast, ast1, ast2, ast3, NULL, srcLoc), "AstMakeFor failed with no post.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeFor(&ast, NULL, NULL, NULL, NULL), "AstMakeFor failed with nothing.");
+    assert_eq(R_OK, AstMakeFor(&ast, NULL, NULL, NULL, NULL, srcLoc), "AstMakeFor failed with nothing.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeFor(NULL, ast1, ast2, ast3, ast4), "AstMakeFor did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeFor(NULL, ast1, ast2, ast3, ast4, srcLoc), "AstMakeFor did not fail.");
 }
 
 TEST(AstMakeWhile) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeWhile(&ast, ast1, ast2), "AstMakeWhile failed.");
+    assert_eq(R_OK, AstMakeWhile(&ast, ast1, ast2, srcLoc), "AstMakeWhile failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeWhile(&ast, ast1, NULL), "AstMakeWhile failed with no body");
+    assert_eq(R_OK, AstMakeWhile(&ast, ast1, NULL, srcLoc), "AstMakeWhile failed with no body");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeWhile(NULL, ast1, ast2), "AstMakeWhile did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeWhile(&ast, NULL, ast2), "AstMakeWhile did not fail with no condition.");
+    assert_eq(R_InvalidArgument, AstMakeWhile(NULL, ast1, ast2, srcLoc), "AstMakeWhile did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeWhile(&ast, NULL, ast2, srcLoc), "AstMakeWhile did not fail with no condition.");
 }
 
 TEST(AstMakeIfElse) {
     struct Ast *ast;
-    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, ast2, ast3), "AstMakeIfElse failed.");
+    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, ast2, ast3, srcLoc), "AstMakeIfElse failed.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, NULL, ast3), "AstMakeIfElse failed with no body.");
+    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, NULL, ast3, srcLoc), "AstMakeIfElse failed with no body.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, ast2, NULL), "AstMakeIfElse failed with no elseif.");
+    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, ast2, NULL, srcLoc), "AstMakeIfElse failed with no elseif.");
     free(ast->Children); free(ast);
-    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, NULL, NULL), "AstMakeIfElse failed with no body or elseif.");
+    assert_eq(R_OK, AstMakeIfElse(&ast, ast1, NULL, NULL, srcLoc), "AstMakeIfElse failed with no body or elseif.");
     free(ast->Children); free(ast);
-    assert_eq(R_InvalidArgument, AstMakeIfElse(NULL, ast1, ast2, ast3), "AstMakeIfElse did not fail.");
-    assert_eq(R_InvalidArgument, AstMakeIfElse(NULL, NULL, ast2, ast3), "AstMakeIfElse did not fail with no condition.");
+    assert_eq(R_InvalidArgument, AstMakeIfElse(NULL, ast1, ast2, ast3, srcLoc), "AstMakeIfElse did not fail.");
+    assert_eq(R_InvalidArgument, AstMakeIfElse(NULL, NULL, ast2, ast3, srcLoc), "AstMakeIfElse did not fail with no condition.");
 }
 
 int main() {
