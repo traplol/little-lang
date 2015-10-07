e61e42e4f3f92323c9650f20ad651c07db8f43f1
Max Mickey <max.mickey1991@gmail.com>
2015-10-07 17:55:31 -0500

    :include/type_info.h - documentation for object methods

--- include/type_info.h
+++ include/type_info.h
@@ -12,6 +12,48 @@ enum TypeInfoType {
     TypeFunction,             /* Functions are first class objects */
 };
 
+/* Every type implements at least the default version of the following methods:
+ *
+ * Arithmetic/math
+ * __add__(self, other)         alias => self + other
+ * __sub__(self, other)         alias => self - other
+ * __mul__(self, other)         alias => self * other
+ * __div__(self, other)         alias => self / other
+ * __mod__(self, other)         alias => self % other
+ * __and__(self, other)         alias => self & other
+ * __or__(self, other)          alias => self | other
+ * __xor__(self, other)         alias => self ^ other
+ * __pow__(self, other)         alias => self ** other
+ * __neg__(self)                alias => - self
+ * __pos__(self)                alias => + self
+ * __lshift__(self, other)      alias => self << other
+ * __rshift__(self, other)      alias => self >> other
+ *
+ * Comparison/logic
+ * __not__(self)                alias => ! self
+ * __eq__(self, other)          alias => self == other
+ * __lt__(self, other)          alias => self < other
+ * __gt__(self, other)          alias => self > other
+ *
+ * Note: __eq__ is expected to return a true or false value
+ * Note: __lt__ is expected to return a true or false value
+ * Note: __eq__ is expected to return a true or false value
+ *
+ * Note: != returns the opposite of __eq__
+ * Note: <= returns __lt__ OR __eq__
+ * Note: >= returns __gt__ OR __eq__
+ *
+ * Misc.
+ * __index__(self, other)       alias => self[other]
+ * __str__(self)                called by => str(thing)
+ * __hash__(self)               called by => hash(thing)
+ * __dbg__(self)                called by => dbg(thing)
+ *
+ * Note: __str__ is expected to return a String object
+ * Note: __hash__ is expected to return an Integer object
+ * Note: __dbg__ is expected to return a String object
+ */
+
 struct TypeInfo {
     enum TypeInfoType Type;
     struct TypeInfo *DerivedFrom; /* All objects are derived from at least the BaseObject including the BaseObject */
