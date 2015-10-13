e7e1bb363acb006f548bd8319913a5942a2f1f96
Max Mickey <max.mickey1991@gmail.com>
2015-10-13 16:00:46 -0500

    :runtime/object.c - removed the old `Object' allocator.

--- runtime/object.c
+++ runtime/object.c
@@ -7,7 +7,6 @@
 
 #include "result.h"
 
-BuiltinFnProc_t RT_Object_NewAllocator;
 static struct SrcLoc srcLoc = {"object.c", -1, -1};
 
 static struct Value *rt_Object___add__(struct Module *module, unsigned int argc, struct Value **argv) {
@@ -92,17 +91,6 @@ static struct Value *rt_Object_is_nan(struct Module *module, unsigned int argc, 
     return &g_TheTrueValue;
 }
 
-static struct Value *rt_Object_NewAllocator(struct Module *module, unsigned int argc, struct Value **argv) {
-    struct Value *value = InterpreterBuildObjectWithDefaults(module, argv[0]->TypeInfo);
-    /* TODO: centralize `#_new_#' */ 
-    struct Value *userReturn = InterpreterDispatchMethod(module, argv[0], "#_new_#", argc, argv, srcLoc);
-    if (value != userReturn) {
-        return userReturn;
-    }
-    return value;
-}
-
-
 #define OBJECT_METHOD_INSERT(name, numArgs, isVarArgs)                  \
     do {                                                                \
         struct Value *method;                                           \
@@ -114,7 +102,6 @@ static struct Value *rt_Object_NewAllocator(struct Module *module, unsigned int 
     } while (0)
 
 int RT_Object_RegisterBuiltins(void) {
-    RT_Object_NewAllocator = rt_Object_NewAllocator;
     OBJECT_METHOD_INSERT(__add__, 2, 0);
     OBJECT_METHOD_INSERT(__sub__, 2, 0);
     OBJECT_METHOD_INSERT(__mul__, 2, 0);
--- runtime/object.h
+++ runtime/object.h
@@ -3,8 +3,6 @@
 
 #include "value.h"
 
-extern BuiltinFnProc_t RT_Object_NewAllocator;
-
 int RT_Object_RegisterBuiltins(void);
 
 #endif
