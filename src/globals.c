e9a75fcbb3012cebc62e941d4aa6bd9a672f609f
Max Mickey <max.mickey1991@gmail.com>
2015-10-13 18:06:54 -0500

    :src/globals.c - implemented macro to define builtin types.

--- src/globals.c
+++ src/globals.c
@@ -4,6 +4,8 @@
 #include "symbol_table.h"
 #include "result.h"
 
+#include "helpers/macro_helpers.h"
+
 #include <stdlib.h>
 
 #define RETURN_ON_FAIL(r)                       \
@@ -31,33 +33,28 @@ struct TypeTable g_TheGlobalTypeTable;
 struct SymbolTable *g_TheGlobalScope;
 struct SymbolTable g_TheUberScope;
 
+static struct SrcLoc srcLoc = {"<globals.c>", -1, -1};
+
+#define MAKE_TYPEINFO_AND_CONSTANT(name, type)                          \
+    do {                                                                \
+        struct TypeInfo *ti = &GLUE3(g_The, name, TypeInfo);            \
+        struct Value *v;                                                \
+        int result = TypeInfoMake(ti, type, &g_TheBaseObjectTypeInfo, XSTR(name)); \
+        RETURN_ON_FAIL(result);                                         \
+        ValueMakeType(&v, ti);                                          \
+        result = SymbolTableInsert(g_TheGlobalScope, v, ti->TypeName, 0, srcLoc); \
+        RETURN_ON_FAIL(result);                                         \
+    } while (0)
+
 static int GlobalsInitTypeInfos(void) {
-    int result;
-    result = TypeInfoMake(&g_TheBaseObjectTypeInfo, TypeBaseObject, 0, "Object");
-    RETURN_ON_FAIL(result);
-    g_TheBaseObjectTypeInfo.DerivedFrom = &g_TheBaseObjectTypeInfo;
-
-    result = TypeInfoMake(&g_TheTypeTypeInfo, TypeType, &g_TheBaseObjectTypeInfo, "Type");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheFunctionTypeInfo, TypeFunction, &g_TheBaseObjectTypeInfo, "Function");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheBuiltinFnTypeInfo, TypeFunction, &g_TheBaseObjectTypeInfo, "BuiltinFn");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheIntegerTypeInfo, TypeInteger, &g_TheBaseObjectTypeInfo, "Integer");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheRealTypeInfo, TypeReal, &g_TheBaseObjectTypeInfo, "Real");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheStringTypeInfo, TypeString, &g_TheBaseObjectTypeInfo, "String");
-    RETURN_ON_FAIL(result);
-
-    result = TypeInfoMake(&g_TheBooleanTypeInfo, TypeBoolean, &g_TheBaseObjectTypeInfo, "Boolean");
-    RETURN_ON_FAIL(result);
-
+    MAKE_TYPEINFO_AND_CONSTANT(BaseObject, TypeBaseObject);
+    MAKE_TYPEINFO_AND_CONSTANT(Type, TypeType);
+    MAKE_TYPEINFO_AND_CONSTANT(Function, TypeFunction);
+    MAKE_TYPEINFO_AND_CONSTANT(BuiltinFn, TypeFunction);
+    MAKE_TYPEINFO_AND_CONSTANT(Integer, TypeInteger);
+    MAKE_TYPEINFO_AND_CONSTANT(Real, TypeReal);
+    MAKE_TYPEINFO_AND_CONSTANT(String, TypeString);
+    MAKE_TYPEINFO_AND_CONSTANT(Boolean, TypeBoolean);
     return R_OK;
 }
 
@@ -112,13 +109,13 @@ int GlobalsInit(void) {
     if (__globalValuesInitialized) {
         return R_GlobalsAlreadyInitted;
     }
+    result = GlobalsInitGlobalScope();
+    RETURN_ON_FAIL(result);
+
     result = GlobalsInitTypeInfos();
     RETURN_ON_FAIL(result);
 
     result = GlobalsInitSingletonValues();
-    RETURN_ON_FAIL(result);
-
-    result = GlobalsInitGlobalScope();
     RETURN_ON_FAIL(result);
 
     result = GlobalsInitGlobalTypeInfo();
