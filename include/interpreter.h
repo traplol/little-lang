c79e990296cd31c9df45f8e8b8d4fa63fc0d9b14
Max Mickey <max.mickey1991@gmail.com>
2015-10-13 22:22:59 -0500

    :include/interpreter.h - exposed some function callers

--- include/interpreter.h
+++ include/interpreter.h
@@ -16,6 +16,9 @@ struct Value *InterpreterRunAst(struct Module *module, struct Ast *ast);
 /* Uses typeinfo to generate the default values for an object */
 struct Value *InterpreterBuildObjectWithDefaults(struct Module *module, struct TypeInfo *typeInfo);
 
+struct Value *InterpreterDoCallBuiltinFn(struct Module *module, struct Value *function, unsigned int argc, struct Value **argv, struct SrcLoc srcLoc);
+struct Value *InterpreterDoCallFunction(struct Module *module, struct Value *function, unsigned int argc, struct Value **argv, struct SrcLoc srcLoc);
+
 /* Attempts to call a method on an object */
 struct Value *InterpreterDispatchMethod(struct Module *module, struct Value *object, char *methodName, unsigned int argc, struct Value **argv, struct SrcLoc srcLoc);
 
