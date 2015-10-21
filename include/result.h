7612e6aa167d401a6cd1958a6c796fecd68f8017
Max Mickey <max.mickey1991@gmail.com>
2015-10-21 01:47:28 -0500

    :include/result.h - changed results to a signed enum.

--- include/result.h
+++ include/result.h
@@ -5,18 +5,38 @@
 #define R_True -1
 #define R_False 0
 
+enum {
+    __RESULT_ENUM_FORCE_SIGNEDNESS__ = -1,
+    R_OK = 0,
+    R_InvalidArgument,
+    R_AllocFailed,
+    R_KeyAlreadyInTable,
+    R_KeyNotInTable,
+    R_UnexpectedToken,
+    R_NotYetImplemented,
+    R_OperationFailed,
+    R_GlobalsAlreadyInitted,
+    R_EndOfTokenStream,
+    R_FileNotFound,
+    R_OutOfMemory,
+    R_MethodNotFound,
+};
+
+/*
 #define R_OK                    0
 #define R_InvalidArgument       1
 #define R_AllocFailed           2
 #define R_KeyAlreadyInTable     3
-#define R_UnexpectedToken       4
-#define R_NotYetImplemented     5
-#define R_OperationFailed       6
-#define R_GlobalsAlreadyInitted 7
-#define R_EndOfTokenStream      8
-#define R_FileNotFound          9
-#define R_OutOfMemory           10
-#define R_MethodNotFound        11
+#define R_KeyNotInTable         4
+#define R_UnexpectedToken       5
+#define R_NotYetImplemented     6
+#define R_OperationFailed       7
+#define R_GlobalsAlreadyInitted 8
+#define R_EndOfTokenStream      9
+#define R_FileNotFound          10
+#define R_OutOfMemory           11
+#define R_MethodNotFound        12
+*/
 
 
 #endif
